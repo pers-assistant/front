a455e37ef0b58255213f1aa5daabdd5b24c65551
Alexey Petrov <adm_sib@mail.ru>
2023-02-06 17:21:18 +0700

    add apps

--- main/src/App.js
+++ main/src/App.js
@@ -1,24 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import MicroFrontend from "./MicroFrontend";
+
+import "./App.css";
+
+
+const {
+  REACT_APP_DIARY_HOST: diaryHost,
+  REACT_APP_FINANCE_HOST: financeHost,
+} = process.env;
+
+function Header() {
+  return (
+      <div className="banner">
+        <h1 className="banner-title">&#128571; Personal Assistant&#128021;</h1>
+        <h4>Random pics of cats and dogs</h4>
+      </div>
+  );
+}
+
+function Diary({ history }) {
+  return <MicroFrontend history={history} host={diaryHost} name="Diary" />;
+}
+
+function Finance({ history }) {
+  return <MicroFrontend history={history} host={financeHost} name="Finance" />;
+}
+
+function GreetingFinance({ history }) {
+  return (
+      <div>
+        <Header />
+        <div className="home">
+          <MicroFrontend history={history} host={financeHost} name="Finance" />
+        </div>
+      </div>
+  );
+}
+
+function Home({ history }) {
+  const [input, setInput] = useState("");
+
+  const handleOnClick = () => {
+    history.push(`/finance/${input}`);
+  };
+
+  return (
+      <div>
+        <Header />
+        <div className="home">
+          <input
+              placeholder="Insert a greeting"
+              defaultValue={input}
+              onBlur={(e) => setInput(e.target.value)}
+          />
+          <button onClick={handleOnClick}>Greet Me</button>
+        </div>
+
+        <div className="home">
+          <div className="content">
+            <div className="finance">
+              <Finance />
+            </div>
+            <div className="diary">
+              <Diary />
+            </div>
+          </div>
+        </div>
+      </div>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <BrowserRouter>
+        <React.Fragment>
+          <Routes>
+            <Route exact path="/" element={<Home/>} />
+            <Route exact path="/diary/:greeting" element={<GreetingFinance/>} />
+          </Routes>
+        </React.Fragment>
+      </BrowserRouter>
   );
 }
 
